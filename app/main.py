b251fe0cdb6f2c6a87e255945784e2f3600b3968
MeghanaRavuri06 <rrmohan.meghana@gmail.com>
2026-01-27 21:55:45 +0530

    Fix CORS

--- app/main.py
+++ app/main.py
@@ -1,30 +1,17 @@
-# app/main.py
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from .model import predict_yield
 
-app = FastAPI(
-    title="Shrimp Yield Prediction API",
-    description="Predicts shrimp yield percentage from pond parameters.",
-)
-
-# CORS so React (localhost:3000) can call FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-
-origins = [
-    "https://frontend-shrimp.vercel.app",  # your frontend deployed URL
-]
+app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://frontend-shrimp.vercel.app"],
-    allow_credentials=False,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
 
 class Features(BaseModel):
     prawn_density_per_m2: float
@@ -43,15 +30,8 @@ class Features(BaseModel):
 
 @app.get("/")
 def root():
-    return {"message": "Shrimp Yield Prediction API is running"}
+    return {"message": "API running"}
 
 @app.post("/predict")
 def predict(features: Features):
-    try:
-        input_dict = features.dict()
-        pred = predict_yield(input_dict)
-        return {"prediction": pred}
-    except ValueError as ve:
-        raise HTTPException(status_code=400, detail=str(ve))
-    except Exception:
-        raise HTTPException(status_code=500, detail="Prediction failed")
+    return {"prediction": predict_yield(features.dict())}
